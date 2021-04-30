06a58c9caa622a3569925175d1875b7c6d68b8fa
Alexandre Martigny <amartigny@devolutions.net>
2021-04-30 12:59:35 -0400

    Set-DSVaultApplications - Help

--- Powershell Module/DevolutionsServer/Public/Vaults/Set-DSVaultApplications.ps1
+++ Powershell Module/DevolutionsServer/Public/Vaults/Set-DSVaultApplications.ps1
@@ -1,9 +1,46 @@
 function Set-DSVaultApplications {
+    <#
+        .SYNOPSIS
+        Sets the allowed applications for a given vault.
+        .DESCRIPTION
+        Sets which application have access to a given vault. If the "Update" flag is present and a supplied application name is already a member of the vault, it will remove this application.
+        .EXAMPLE
+        No update flag, no applications allowed
+        Current applications allowed in vault:
+        None
+
+        Set-DSVaultApplications @("App1", "App2")
+        -> Allowed applications: App1, App2
+        .EXAMPLE
+        No update flag, some applications allowed
+        Current applications allowed in vault:
+        App1, App2
+
+        Set-DSVaultApplications @("App3")
+        -> Allowed applications: App3
+        .EXAMPLE
+        Update flag present, some applications allowed (Add another)
+        Current applications allowed in vault:
+        App1
+
+        Set-DSVaultApplications @("App2") -Update 
+        -> Allowed applications: App1, App2
+        .EXAMPLE
+        Update flag present, some applications allowed (Remove an application)
+        Current applications allowed in vault:
+        App1, App2
+
+        Set-DSVaultApplications @("App2", "App3") -Update 
+        -> Allowed applications: App1, App3
+    #>
     [CmdletBinding()]
     PARAM (
         [ValidateNotNullOrEmpty()]
+        #Vault's ID to update
         [guid]$VaultID,
+        #String array with application names (Not ID's) to allow in vault
         [string[]]$AllowedApplicationsList,
+        #Used to know if we're creating a vault or updating a currently existing one
         [switch]$Update
     )
 
