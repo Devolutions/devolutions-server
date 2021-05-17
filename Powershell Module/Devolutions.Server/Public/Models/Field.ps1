26b9a48b3fce73a5379ad271cb40264c914abe85
Alexandre Martigny <amartigny@devolutions.net>
2021-05-17 11:08:44 -0400

    Renamed NewParam for Field

---
+++ Powershell Module/Devolutions.Server/Public/Models/Field.ps1
@@ -0,0 +1,27 @@
+<#
+    .SYNOPSIS
+    Definition for 'Field' custom object
+    .DESCRIPTION
+    'Field' objects are used to add new unsupported (module-wise) fields to a DS object (RDP, SSH Shell...).
+    .EXAMPLE
+    [Field]$NewField1 = [Field]::new('NewlySupportedField1', 'ThisIsANewField1', 'root')
+    [Field]$NewField2 = [Field]::new('NewlySupportedField2', 'ThisIsANewField2', 'data')
+    [Field]$NewField3 = [Field]::new('NewlySupportedField3', 'ThisIsANewField3', 'events')
+    New-DSRDP @($NewField1, $NewField2, $NewField3)
+#>
+
+class Field {
+    #New field's name
+    [string]$Name
+    #New field's value
+    [string]$Value
+    #New field's depth in object
+    [ValidateSet('root', 'data', 'events')]
+    [string]$Depth
+    
+    NewParam([string]$Name, [string]$Depth, [string]$Value) {
+        $this.Name = $Name
+        $this.Depth = $Depth
+        $this.Value = $Value
+    }
+}
