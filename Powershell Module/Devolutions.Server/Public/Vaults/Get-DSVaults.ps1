6e9045b220eaa378bd3ea8dc640952439ded3fdd
Alexandre Martigny <amartigny@devolutions.net>
2021-05-21 11:20:58 -0400

    Get-DSVaults Pagination
    -Made default page size 100
    -Added PageNumer and PageSize to PSBoundParameters (Default values are not in PSBoundParameters
    -Now loop all pages and return one array with all vaults without pagination data

--- Powershell Module/Devolutions.Server/Public/Vaults/Get-DSVaults.ps1
+++ Powershell Module/Devolutions.Server/Public/Vaults/Get-DSVaults.ps1
@@ -1,4 +1,4 @@
-function Get-DSVaults{
+function Get-DSVaults {
     <#
     .SYNOPSIS
     
@@ -10,48 +10,70 @@ function Get-DSVaults{
     
     .LINK
     #>
-        [CmdletBinding()]
-        param(			
-            [ValidateSet("Name", "Description")]
-            [string]$SortField = '',
-            [System.Management.Automation.SwitchParameter]$Descending,
-            [int]$PageNumber = 1,
-            [int]$PageSize = 25,
-            [Switch]$Legacy 
-        )
+    [CmdletBinding()]
+    param(			
+        [ValidateSet('Name', 'Description')]
+        [string]$SortField = '',
+        [System.Management.Automation.SwitchParameter]$Descending,
+        [int]$PageNumber = 1,
+        [int]$PageSize = 100,
+        [Switch]$Legacy 
+    )
         
-        BEGIN {
-            Write-Verbose '[Get-DSVaults] begin...'
-            if ([string]::IsNullOrWhiteSpace($Global:DSSessionToken))
-			{
-				throw "Session does not seem authenticated, call New-DSSession."
-			}
-          }
-    
-        PROCESS {
-            [ServerResponse]$response = $null
-
-            $LegacyRequested = $Legacy.IsPresent
-            $PSBoundParameters.Remove('Legacy') | out-null
-
-            [System.Version]$ModernVersion = '2020.3.8.0'
-            [System.Version]$v = $Global:DSInstanceVersion
-            
-            if (($LegacyRequested) -or ($v.CompareTo($ModernVersion) -lt 0)){
-                $response = Get-DSVaultsLegacy @PSBoundParameters
-            }
-            else {
-                $response = Get-DSVaultsModern @PSBoundParameters
-            }
-
-            return $response
-        }
-    
-        END {
-           If ($?) {
-              Write-Verbose '[Get-DSVaults] Completed Successfully.'
-            } else {
-                Write-Verbose '[Get-DSVaults] ended with errors...'
-            }
+    BEGIN {
+        Write-Verbose '[Get-DSVaults] Beginning...'
+        if ([string]::IsNullOrWhiteSpace($Global:DSSessionToken)) {
+            throw 'Session does not seem authenticated, call New-DSSession.'
         }
     }
+    
+    PROCESS {
+        [System.Collections.ArrayList]$Vaults = @()
+        [ServerResponse]$response = $null
+
+        $LegacyRequested = $Legacy.IsPresent
+        $PSBoundParameters.Remove('Legacy') | out-null
+
+        if (!$PSBoundParameters.PageNumber) { $PSBoundParameters.Add('PageNumber', $PageNumber) }
+        if (!$PSBoundParameters.PageSize) { $PSBoundParameters.Add('PageSize', $PageSize) }
+
+        [System.Version]$ModernVersion = '2020.3.8.0'
+        [System.Version]$v = $Global:DSInstanceVersion
+            
+        if (($LegacyRequested) -or ($v.CompareTo($ModernVersion) -lt 0)) {
+            do {
+                $response = Get-DSVaultsLegacy @PSBoundParameters
+                $response.Body.data | ForEach-Object {
+                    $Vaults += $_
+                }
+                $PageNumber++
+            } while ($PageNumber -le $response.Body.totalPage)    
+        }
+        else {
+            do {
+                $response = Get-DSVaultsModern @PSBoundParameters
+                $response.Body.data | ForEach-Object {
+                    $Vaults += $_
+                }
+                $PageNumber++
+                $PSBoundParameters.PageNumber++
+            } while ($PageNumber -le $response.Body.totalPage)
+        }
+
+        $response.Body.data = $Vaults
+        $response.Body.PSObject.Properties.Remove('currentPage')
+        $response.Body.PSObject.Properties.Remove('pageSize')
+        $response.Body.PSObject.Properties.Remove('totalCount')
+        $response.Body.PSObject.Properties.Remove('totalPage')
+        return $response
+    }
+    
+    END {
+        If ($response.isSuccess) {
+            Write-Verbose '[Get-DSVaults] Completed Successfully.'
+        }
+        else {
+            Write-Verbose '[Get-DSVaults] Ended with errors...'
+        }
+    }
+}
