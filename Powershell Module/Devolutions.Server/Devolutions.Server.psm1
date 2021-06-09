80d7c50b099d7aeac75adbc2af6c35a797694ff8
Alexandre Martigny <amartigny@devolutions.net>
2021-06-09 14:06:27 -0400

    Manifest
    Moved enum type definition above other cmdlet import so they can be imported properly (And we know if we are missing an enum because it crashes if it cant find the type)

--- Powershell Module/Devolutions.Server/Devolutions.Server.psm1
+++ Powershell Module/Devolutions.Server/Devolutions.Server.psm1
@@ -6,37 +6,36 @@ $Manifest = Import-PowerShellDataFile -Path $(Join-Path $PSScriptRoot "${ModuleN
 Export-ModuleMember -Cmdlet @($manifest.CmdletsToExport)
 
 $Public = @(Get-ChildItem -Path "$PSScriptRoot/Public/*.ps1" -Recurse)
-$Private = @(Get-ChildItem -Path "$PSScriptRoot/Private/*.ps1" -Recurse)
+$Private = @(Get-ChildItem -Path "$PSScriptRoot/Private/*.ps1" -Recurse | Where-Object { $_.FullName -inotmatch 'enums' })
 $Deprecate = @(Get-ChildItem -Path "$PSScriptRoot/Deprecate/*.ps1" -Recurse)
+$Enums = @(Get-ChildItem -Path "$PSScriptRoot/Private/Types/enums/*.ps1" -Recurse)
+
+#Load enums as types before loading cmdlets
+foreach ($Enum in $Enums) {
+    $FileContent = Get-Content $Enum.FullName
+    [object[]]$typedef = [object[]]::new($FileContent.Length)
+
+    #Need to add public identifier before enum declaration
+    $typedef.Item(0) = "public $($FileContent[0])"
+
+    foreach ($Line in $FileContent) {
+        if ($Line -ne $FileContent[0]) {
+            #Need to add comma to enum values if current value isnt last
+            if (($Line.Contains('=')) -and ($FileContent[$FileContent.IndexOf($Line) + 1].ToString().Contains('='))) {
+                $typedef.Item($FileContent.IndexOf($Line)) = $Line + ','
+            }
+            else {
+                $typedef.Item($FileContent.IndexOf($Line)) = $Line
+            }
+        }
+    }
+    #This loads the type in global scope so it can be used outside of this script.
+    Add-Type -TypeDefinition ($typedef | Out-String)
+}
 
 foreach ($Import in @($Public + $Private + $Deprecate)) {
     try {
-        <# Needed to add enums and validators to global scope. Dot sourcing didnt work, we need to "AddType".
-        Other solution is .ps1xml (type definitions) but this is easier and doesn't take long #>
-        if ($Import.Directory.Name -in @('enums')) {
-            $FileContent = Get-Content $Import.FullName
-            [object[]]$typedef = [object[]]::new($FileContent.Length)
-
-            #Need to add public identifier before enum declaration
-            $typedef.Item(0) = "public $($FileContent[0])"
-
-            foreach ($Line in $FileContent) {
-                if ($Line -ne $FileContent[0]) {
-                    #Need to add comma to enum values if current value isnt last
-                    if (($Line.Contains('=')) -and ($FileContent[$FileContent.IndexOf($Line) + 1].ToString().Contains('='))) {
-                        $typedef.Item($FileContent.IndexOf($Line)) = $Line + ','
-                    }
-                    else {
-                        $typedef.Item($FileContent.IndexOf($Line)) = $Line
-                    }
-                }
-            }
-            #This loads the type in global scope so it can be used outside of this script.
-            Add-Type -TypeDefinition ($typedef | Out-String)
-        }
-        else {
-            . $Import.FullName
-        }
+            . $Import.FullName 
     }
     catch {
         Write-Error -Message "Failed to import function $($Import.FullName): $_"
@@ -46,19 +45,3 @@ foreach ($Import in @($Public + $Private + $Deprecate)) {
 foreach ($file in $Public) {
     Export-ModuleMember -Function $file.BaseName
 }
-
-<#
-foreach ($file in $Enums ) {
-    $FileContent = Get-Content $file.FullName | Out-String
-
-    Add-Type -TypeDefinition @"
-    public enum AddEntryMode
-    {
-    Default = 0,
-    TemplateListWithBlank = 1,
-    TemplateListOnly = 2,
-    NoTemplate = 3
-    }
-"@
-}
-#>
