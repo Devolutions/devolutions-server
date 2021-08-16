def8aba378ed9d9104331498ed5b25c580debf3c
Alexandre Martigny <amartigny@devolutions.net>
2021-08-16 14:13:09 -0400

    Added approverID and reason to Invoke-Checkout

--- Powershell Module/Devolutions.Server/Public/Pam/Checkout/Invoke-DSPamCheckout.ps1
+++ Powershell Module/Devolutions.Server/Public/Pam/Checkout/Invoke-DSPamCheckout.ps1
@@ -1,7 +1,9 @@
 function Invoke-DSPamCheckout {
     [CmdletBinding()]
     PARAM (
-        [guid]$PamCredentialID = $(throw 'You must provide a valid ID.')
+        [guid]$PamCredentialID = $(throw 'You must provide a valid ID.'),
+        [string]$Reason,
+        [guid]$ApproverID
     )
     
     BEGIN {
@@ -13,26 +15,50 @@ function Invoke-DSPamCheckout {
     }
     
     PROCESS {
-        #1. Fetch PAM cred (api/pam/credentials/$id)
+        #1. Fetch PAM cred (api/pam/credentials/$id) 
         $PamCredential = ($res = Get-DSPamCredential $PamCredentialID).isSuccess ? ($res.Body) : $(throw 'Failed while fetching credentials. Please make sure the ID you provided is a valid PAM account ID and try again.')
+    
+        #1.1 Check for approver
+        if (($PamCredential.checkoutApprovalMode -eq 2) -and !$ApproverID) {
+            Write-Error 'This entry requires an approver to check out. Please provide an approver ID and try again.'
+        }
+
+        #1.2 Check for reason mode
+        if (($PamCredential.checkoutReasonMode -eq 2) -and !$Reason) {
+            Write-Error 'This entry requires a reason to check out. Please provide a reason and try again.'
+        }
 
         #2. Checkout
         $RequestParams = @{
             URI    = "$Script:DSBaseURI/api/pam/checkouts"
             Method = 'POST'
-            Body   = (ConvertTo-Json @{credentialID = $PamCredentialID; duration = $PamCredential.duration})
+            Body   = @{credentialID = $PamCredentialID; duration = 240 }
         }
+
+        if ($PamCredential.checkoutApprovalMode -eq 2) {
+            $RequestParams.Body += @{approverID = $ApproverID }
+        }
+
+        if ($PamCredential.checkoutReasonMode -in @(2, 3)) {
+            $RequestParams.Body += @{reason = $Reason }
+        }
+
+        $RequestParams.Body = ConvertTo-Json $RequestParams.Body
 
         [PamCheckout]$CredentialCheckout = ($res = Invoke-DS @RequestParams).isSuccess ? ($res.Body) : ($res.ErrorMessage ? $(throw $res.ErrorMessage) : $(throw 'Failed while trying to checkout the account. See logs for information.'))
 
-        #3. Get Password
-        $EncryptedPassword = ($res = Get-DSPamPassword $PamCredentialID).isSuccess ? ($res.Body) : ($res.ErrorMessage ? $(throw $res.ErrorMessage): $(throw 'Failed while fetching password. See logs for information.'))
+        if ($CredentialCheckout.Status -ne 1) {
+            #3. Get Password
+            $EncryptedPassword = ($res = Get-DSPamPassword $PamCredentialID).isSuccess ? ($res.Body) : ($res.ErrorMessage ? $(throw $res.ErrorMessage): $(throw 'Failed while fetching password. See logs for information.'))
 
-        #4. Decrypt password
-        $DecryptedPassword = Decrypt-String $Global:DSSessionKey $EncryptedPassword
+            #4. Decrypt password
+            $DecryptedPassword = Decrypt-String $Global:DSSessionKey $EncryptedPassword
 
-        #5. Return checkout info and decrypted password
-        return @{CheckoutInfo = $CredentialCheckout; Password = $DecryptedPassword }
+            #5. Return checkout info and decrypted password
+            return @{CheckoutInfo = $CredentialCheckout; Password = $DecryptedPassword }
+        }
+        
+        return $CredentialCheckout
     }
     
     END {
