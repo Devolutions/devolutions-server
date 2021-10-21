3eba318eec0c52ceeac6a58aa9818d0f5fd62c76
Eric St-Martin <estmartin@devolutions.net>
2021-10-21 12:04:54 -0400

    Install-DevolutionsInstance - Remove JSON file deletion post-install

--- HelperModule/Public/Install-DevolutionsInstance.ps1
+++ HelperModule/Public/Install-DevolutionsInstance.ps1
@@ -11,14 +11,5 @@
         Return
     } else {
         .\DPS.Console.CLI.exe @$JSON --verbose
-        try {
-            Write-LogEvent "Removing $JSON from $env:COMPUTERNAME"
-            Remove-Item $JSON
-        } catch [System.Exception] { Write-LogEvent $_ -Errors }
     }
-    <# Used for removing the ZIP. Took off for now as I believe we should keep the zip in case.
-    try {
-        Write-LogEvent "Removing $path\Packages\DVLS.Instance.zip from $env:COMPUTERNAME"
-        Remove-Item "$path\Packages\DVLS.Instance.zip"
-    } catch [System.Exception] { Write-LogEvent $_ -Errors } #>
 }
