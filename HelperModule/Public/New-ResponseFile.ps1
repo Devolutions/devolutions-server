c4cace9fc288ec000790bdfcd623061306c89514
Eric St-Martin <estmartin@devolutions.net>
2021-10-21 12:04:29 -0400

    New-ResponseFile Removed extra lines to params

--- HelperModule/Public/New-ResponseFile.ps1
+++ HelperModule/Public/New-ResponseFile.ps1
@@ -4,66 +4,52 @@
         [Parameter(Mandatory, ParameterSetName = 'Credential', Position = 0)]
         [System.Management.Automation.PSCredential]$DatabaseMasterAccount,
 
-        [Parameter(Mandatory, ParameterSetName = 'Credential', Position = 1)]
+        [Parameter(ParameterSetName = 'Credential', Position = 1)]
         [System.Management.Automation.PSCredential]$SchedulerAccount,
 
-        [Parameter(Mandatory, ParameterSetName = 'Credential', Position = 2)]
+        [Parameter(ParameterSetName = 'Credential', Position = 2)]
         [System.Management.Automation.PSCredential]$VaultRunnerAccount,
 
-        [Parameter(Mandatory, ParameterSetName = 'Credential', Position = 5, HelpMessage = 'Include full format license key from email.')]
-        [Parameter(Mandatory, ParameterSetName = 'Integrated', Position = 1, HelpMessage = 'Include full format license key from email.')]
+        [Parameter(Mandatory, HelpMessage = 'Include full format license key from email.')]
         [string]$LicenseKey,
 
-        [Parameter(Mandatory, ParameterSetName = 'Credential', Position = 3, HelpMessage = 'Devolutions Server account used for  first time login account.')]
-        [Parameter(Mandatory, ParameterSetName = 'Integrated', Position = 2, HelpMessage = 'Devolutions Server account used for first time login account.')]
+        [Parameter(Mandatory, HelpMessage = 'Devolutions Server account used for  first time login account.')]
         [System.Management.Automation.PSCredential]$DevolutionsAdminAccount,
 
-        [Parameter(Mandatory, ParameterSetName = 'Credential', Position = 6, HelpMessage = 'Password will be used for backup encryption keys.')]
-        [Parameter(Mandatory, ParameterSetName = 'Integrated', Position = 3, HelpMessage = 'Password will be used for backup encryption keys.')]
+        [Parameter(Mandatory, HelpMessage = 'Password will be used for backup encryption keys.')]
         [System.Management.Automation.PSCredential]$EncryptionKeysPassword,
 
-        [Parameter(Mandatory, ParameterSetName = 'Credential', HelpMessage = 'Email that will be stored on Devolutions Server first time login account.')]
-        [Parameter(Mandatory, ParameterSetName = 'Integrated', HelpMessage = 'Email that will be stored on Devolutions Server first time login account.')]
+        [Parameter(Mandatory, HelpMessage = 'Email that will be stored on Devolutions Server first time login account.')]
         [string]$DevolutionsAdminEmail,
 
-        [Parameter(Mandatory, ParameterSetName = 'Credential', HelpMessage = 'IE: localhost\SQLEXPRESS')]
-        [Parameter(Mandatory, ParameterSetName = 'Integrated', HelpMessage = 'IE: localhost\SQLEXPRESS')]
+        [Parameter(Mandatory, HelpMessage = 'IE: localhost\SQLEXPRESS')]
         [string]$SqlServerHost,
 
-        [Parameter(ParameterSetName = 'Credential', HelpMessage = 'Name of that Database that will be created.')]
-        [Parameter(ParameterSetName = 'Integrated', HelpMessage = 'Name of that Database that will be created.')]
+        [Parameter(HelpMessage = 'Name of that Database that will be created.')]
         [string]$DatabaseName = 'dvls',
 
-        [Parameter(ParameterSetName = 'Credential', HelpMessage = 'Name of that Devolutions Server that will be shown on the Console.')]
-        [Parameter(ParameterSetName = 'Integrated', HelpMessage = 'Name of that Devolutions Server that will be shown on the Console.')]
+        [Parameter(HelpMessage = 'Name of that Devolutions Server that will be shown on the Console.')]
         [string]$ServerName = 'Devolutions Server',
 
-        [Parameter(ParameterSetName = 'Credential', HelpMessage = 'Will disable HTTPs use on the server. Note: Not recommended for production environments.')]
-        [Parameter(ParameterSetName = 'Integrated', HelpMessage = 'Will disable HTTPs use on the server. Note: Not recommended for production environments.')]
+        [Parameter(HelpMessage = 'Will disable HTTPs use on the server. Note: Not recommended for production environments.')]
         [switch]$DisableHttps,
 
-        [Parameter(ParameterSetName = 'Credential', HelpMessage = 'Will disable Encryption on Config. Note: Not recommended for production environments.')]
-        [Parameter(ParameterSetName = 'Integrated', HelpMessage = 'Will disable Encryption on Config. Note: Not recommended for production environments.')]
+        [Parameter(HelpMessage = 'Will disable Encryption on Config. Note: Not recommended for production environments.')]
         [switch]$DisableConfigEncryption,
 
-        [Parameter(ParameterSetName = 'Credential', HelpMessage = 'Location backup encryption keys will be stored')]
-        [Parameter(ParameterSetName = 'Integrated', HelpMessage = 'Location backup encryption keys will be stored')]
+        [Parameter(HelpMessage = 'Location backup encryption keys will be stored')]
         [string]$BackupKeysLocation = 'C:\DPS Key Backup\DVLS\',
 
-        [Parameter(ParameterSetName = 'Credential', HelpMessage = 'Name that will be show on IIS.')]
-        [Parameter(ParameterSetName = 'Integrated', HelpMessage = 'Name that will be show on IIS.')]
+        [Parameter(HelpMessage = 'Name that will be show on IIS.')]
         [string]$AppPoolName = 'dvls',
 
-        [Parameter(ParameterSetName = 'Credential', HelpMessage = 'Name that will be use on Webpage.')]
-        [Parameter(ParameterSetName = 'Integrated', HelpMessage = 'Name that will be use on Webpage.')]
+        [Parameter(HelpMessage = 'Name that will be use on Webpage.')]
         [string]$WebAppName = 'dvls',
 
-        [Parameter(ParameterSetName = 'Credential', HelpMessage = 'Location of local Devolutions ZIP for installation.')]
-        [Parameter(ParameterSetName = 'Integrated', HelpMessage = 'Location of local Devolutions ZIP for installation.')]
+        [Parameter(HelpMessage = 'Location of local Devolutions ZIP for installation.')]
         [string]$ZIPFileLocation = 'null',
 
-        [Parameter(ParameterSetName = 'Credential', HelpMessage = 'Set Server description.')]
-        [Parameter(ParameterSetName = 'Integrated', HelpMessage = 'Set Server description.')]
+        [Parameter(HelpMessage = 'Set Server description.')]
         [string]$ServerDescription = 'null',
 
         [parameter(Mandatory, ParameterSetName = 'Integrated', Position = 0)]
@@ -82,12 +68,6 @@
     if ($ZIPFileLocation -ne 'null') { $ZIPInstall = '"install-zip": ' + '"' + "$ZIPFileLocation" + '"' + ',' } else { $ZIPInstall = '"install-zip": null,' }
     if ($ServerDescription -ne 'null') { $Description = '"description": ' + '"' + "$ServerDescription" + '"' + ',' } else { $Description = '"description": null,' }
 
-    #TODO Too many prompts not able to use Install-DevolutionsServer to set the accounts.
-
-    <#TODO Scheduler Service, App pool indentity type w/user&pass, service account w/user&pass,
-        DPS-Website-name/dps-path, console-pwd/disable-password
-    #>
-
     $path = Split-Path -Path $PSScriptRoot -Parent
     $adminUser = $DevolutionsAdminAccount.GetNetworkCredential().UserName
     $adminPass = $DevolutionsAdminAccount.GetNetworkCredential().Password
@@ -97,11 +77,21 @@
     if (!($IntSec)) {
         $DBownerUser = $DatabaseMasterAccount.GetNetworkCredential().UserName
         $DBownerPass = $DatabaseMasterAccount.GetNetworkCredential().Password
-        $SchedulerUser = $SchedulerAccount.GetNetworkCredential().UserName
-        $SchedulerPass = $SchedulerAccount.GetNetworkCredential().Password
-        $VaultRunnerUser = $VaultRunnerAccount.GetNetworkCredential().UserName
-        $VaultRunnerPass = $VaultRunnerAccount.GetNetworkCredential().Password
-
+        if ($null -eq $SchedulerAccount) {
+            $SchedulerUser = $DatabaseMasterAccount.GetNetworkCredential().UserName
+            $SchedulerPass = $DatabaseMasterAccount.GetNetworkCredential().Password
+        } else {
+            $SchedulerUser = $SchedulerAccount.GetNetworkCredential().UserName
+            $SchedulerPass = $SchedulerAccount.GetNetworkCredential().Password 
+        } 
+        if ($null -eq $VaultRunnerAccount) {
+            $VaultRunnerUser = $DatabaseMasterAccount.GetNetworkCredential().UserName
+            $VaultRunnerPass = $DatabaseMasterAccount.GetNetworkCredential().Password
+        } else {
+            $VaultRunnerUser = $VaultRunnerAccount.GetNetworkCredential().UserName
+            $VaultRunnerPass = $VaultRunnerAccount.GetNetworkCredential().Password
+        }
+        
         $response = @"
 {
 "accept-eula": true,
