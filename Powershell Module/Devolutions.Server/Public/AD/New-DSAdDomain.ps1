4359619146f5a67a013bc1363ea4bb7a5eb07cab
Alexandre Martigny <amartigny@devolutions.net>
2021-11-18 09:09:51 -0500

    Added other basic settings

--- Powershell Module/Devolutions.Server/Public/AD/New-DSAdDomain.ps1
+++ Powershell Module/Devolutions.Server/Public/AD/New-DSAdDomain.ps1
@@ -1,44 +1,69 @@
 function New-DSAdDomain {
     [CmdletBinding()]
     param (
-        [string]$DomainName,
-        [string]$DisplayName,
-        [string]$AdministrationUsername,
-        [string]$AdministrationPassword,
+        [ValidateNotNullOrEmpty()][string]$DomainName,
+        [ValidateNotNullOrEmpty()][string]$DisplayName,
+        [ValidateNotNullOrEmpty()][string]$AdministrationUsername,
+        [ValidateNotNullOrEmpty()][string]$AdministrationPassword,
 
         [bool]$IsLDAPS,
         [int]$LdapsCustomPort,
 
         [bool]$AutoCreateEnabled = $false,
-        [DomainUsernameFormatType]$AutoCreateUsernameFormatType,
-        [bool]$AutoCreateReadOnly,
-        [string]$AutoCreateVault
-
+        [DomainUsernameFormatType]$DomainUsernameFormatType,
+        [bool]$DomainUserReadOnly,
+        [string]$DefaultVault
     )
     
     begin {
         Write-Verbose '[New-DSAdDomain] Beginning...'
         
         if ([string]::IsNullOrWhiteSpace($Global:DSSessionToken)) {
-            throw "Session invalid. Please call New-DSSession."
+            throw 'Session invalid. Please call New-DSSession.'
         }
     }
     
     process {
         $MsDomainConfiguration = [MsDomainConfiguration]::new()
+
         $MsDomainConfiguration.DomainName = $DomainName
         $MsDomainConfiguration.DisplayName = $DisplayName
         $MsDomainConfiguration.AdministrationUsername.Set($AdministrationUsername)
         $MsDomainConfiguration.AdministrationPassword.Set((Protect-ResourceToHexString $AdministrationPassword))
 
+        if ($PSBoundParameters.ContainsKey('IsLdaps')) {
+            $MsDomainConfiguration.IsLdaps.Set($IsLDAPS)
+
+            $PSBoundParameters.ContainsKey('LdapsCustomPort') ? $MsDomainConfiguration.LdapsCustomPort.Set($LdapsCustomPort) : [void]
+        }
+
+        if ($PSBoundParameters.ContainsKey('AutoCreateEnabled') -and $AutoCreateEnabled) {
+            $MsDomainConfiguration.AutoCreateEnabled.Set($AutoCreateEnabled)
+
+            if ($PSBoundParameters.ContainsKey('DefaultVault')) {
+                $VaultId = ($res = Get-DSVault -All).isSuccess ? 
+                    ($res.Body.data | Where-Object { $_.Name -eq $DefaultVault } | Select-Object -ExpandProperty id) : 
+                    ([guid]::Empty())
+
+                if ($VaultId -eq ([guid]::Empty)) {
+                    Write-Verbose "[New-DSAdDomain] $DefaultVault could not be found. Devolutions Server's default vault used instead."
+                }
+
+                $MsDomainConfiguration.AutoCreateVault.Set($VaultId)
+            }
+            
+            $PSBoundParameters.ContainsKey('DomainUsernameFormatType') ? $MsDomainConfiguration.AutoCreateUsernameFormatType.Set($DomainUsernameFormatType) : ([void] | Out-Null)
+            $PSBoundParameters.ContainsKey('DomainUserReadOnly') ? $MsDomainConfiguration.AutoCreateReadOnly.Set($DomainUserReadOnly) : ([void] | Out-Null)
+        }
+
         $RequestParams = @{
-            URI = "$Script:DSBaseURI/api/active-directory/domain-configurations"
+            URI    = "$Script:DSBaseURI/api/active-directory/domain-configurations"
             Method = 'POST'
-            Body = (ConvertTo-Json $MsDomainConfiguration)
+            Body   = (ConvertTo-Json $MsDomainConfiguration)
         }
 
         $res = Invoke-DS @RequestParams
-        $res
+        return $res
     }
     
     end {
