c8d772d63896898c2b38830fe7ff7bc59a1a37aa
Alexandre Martigny <amartigny@devolutions.net>
2021-11-22 15:06:28 -0500

    Handling names inside cmdlet
    - Resolving names inside cmdlet instead of asking user to add '|u'
    - Now remove name if it cant be found (user or user group)
    - Also added help section

--- Powershell Module/Devolutions.Server/Public/Utils/Permissions/Set-DSEntityPermissions.ps1
+++ Powershell Module/Devolutions.Server/Public/Utils/Permissions/Set-DSEntityPermissions.ps1
@@ -1,4 +1,32 @@
 function Set-DSEntityPermissions {
+    <#
+        .SYNOPSIS
+        Sets the permissions on a given entity.
+        .DESCRIPTION
+        Sets the permissions on a given entity (Vault, folder or entry).
+        .EXAMPLE
+        $Permissions = @(
+            [ConnectionPermission]@{
+                IsEmpty  = $false
+                Override = [SecurityRoleOverride]::Custom
+                Right    = [SecurityRoleRight]::View
+                Roles    = @('User1', 'User2', 'Group1', 'Group2')
+            },
+            [ConnectionPermission]@{
+                IsEmpty  = $false
+                Override = [SecurityRoleOverride]::Inherited
+                Right    = [SecurityRoleRight]::Edit
+                Roles    = @('User1', 'User2', 'Group1', 'Group2')
+            }
+        )
+
+        > Set-DSEntityPermissions -EntityId $ID -Permissions $Permissions
+
+        .NOTES
+        - To override already existing permissions, please use the 'OverrideExistingPermissions' switch parameter.
+        - When using this CMDlet, your entity's override mode will default to 'Custom'. Use the 'PermissionOverride' parameter if you wish to set it to anything else.
+        .NOTES
+        #>
     [CmdletBinding()]
     param (
         [guid]$EntityId = $(throw 'You must provide the ID of the entity for which you want to change the permissions.'),
@@ -19,6 +47,9 @@ function Set-DSEntityPermissions {
         $Entry = ($res = Get-DSEntry -EntryId $EntityId).isSuccess ? $res.Body.data : $(throw 'Could not find the requested entity.')
         $EntrySecurity = $Entry.security
 
+        $Users = ($res = Get-DSUsers -All).isSuccess ? $res.Body.data : $(throw 'Could not fetch your users.')
+        $UserGroups = ($res = Get-DSRole -GetAll).isSuccess ? $res.Body.data : $(throw 'Could not fetch your user groups.')
+
         if (!$EntrySecurity.permissions) {
             $EntrySecurity | Add-Member -NotePropertyName permissions -NotePropertyValue @()
         }
@@ -26,6 +57,17 @@ function Set-DSEntityPermissions {
         $EntrySecurity | Add-Member -NotePropertyName roleOverride -NotePropertyValue $PermissionOverride -Force
 
         foreach ($Permission in $Permissions) {
+            foreach ($Role in $Permission.Roles) {
+                if ($null -ne ($User = $Users.GetEnumerator() | Where-Object { $_.name -eq $Role })) {
+                    $Permission.Roles[$Permission.Roles.IndexOf($Role)] = "$Role|u"
+                }
+                elseif ($null -eq ($UserGroup = $UserGroups.GetEnumerator() | Where-Object { $_.name -eq $Role })) {
+                    Write-Verbose "[Set-DSEntityPermissions] $Role was removed from $($Permission.Right) permission because it couldn't be located in your users or user groups."
+                    $Permission.Roles = $Permission.Roles | Where-Object { $_ -ne $Role }
+                }
+
+            }
+
             $Right = $EntrySecurity.permissions.GetEnumerator() | Where-Object { $_.Right -eq $Permission.right }
 
             if ($Right -and !($Permission.Right -eq [SecurityRoleRight]::View)) {
