348c5ab52733238d34d8d053c38b50d4df1fe499
Alexandre Martigny <amartigny@devolutions.net>
2022-12-02 08:19:11 -0500

    Fixed missing enum from exports

---
+++ Powershell Module/Devolutions.Server/Private/Types/enums/CheckoutStatus.generated.psm1
@@ -0,0 +1,13 @@
+enum CheckoutStatus
+{
+    Invalid = 0
+    Pending = 1
+    Granted = 2
+    Denied = 3
+    Active = 4
+    Ended = 5
+    Expired = 6
+    Aborted = 7
+    RequestCanceled = 8
+    Initial = 9
+}
