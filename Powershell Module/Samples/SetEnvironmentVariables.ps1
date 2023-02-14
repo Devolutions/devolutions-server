8439b9982665e683298a597094d82782c74aa80e
Érica Poirier <epoirier@devolutions.net>
2023-02-14 14:48:34 -0500

    Update SetEnvironmentVariables.ps1

--- Powershell Module/Samples/SetEnvironmentVariables.ps1
+++ Powershell Module/Samples/SetEnvironmentVariables.ps1
@@ -1,6 +1,6 @@
 #the samples expect that credentials and the url to your server be defined
 #in environment variables.  Since storing creds in scripts is frowned upon,
 #adapt a local file for running your own tests.
-$env:DS_URL= 'http://localhost/dps'
+$env:DS_URL= 'https://localhost/dvls'
 $env:DS_USER = '{your user here}'
 $env:DS_PASSWORD = '{your password here}'
